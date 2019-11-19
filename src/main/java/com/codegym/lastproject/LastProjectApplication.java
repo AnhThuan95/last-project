cf205fb3f95860a13d35db26226cfd08a5f4e427
ductu1411 <ductu1411@gmail.com>
2019-11-19 09:17:54 +0700

    Tạo formatter và tiêm vào application

--- src/main/java/com/codegym/lastproject/LastProjectApplication.java
+++ src/main/java/com/codegym/lastproject/LastProjectApplication.java
@@ -1,12 +1,24 @@
 package com.codegym.lastproject;
 
+import com.codegym.lastproject.formatter.CategoryFormatter;
+import com.codegym.lastproject.service.CategoryService;
+import com.codegym.lastproject.service.HomeService;
 import com.codegym.lastproject.service.RoleService;
 import com.codegym.lastproject.service.UserService;
+import com.codegym.lastproject.service.impl.CategoryServiceImpl;
+import com.codegym.lastproject.service.impl.HomeServiceImpl;
 import com.codegym.lastproject.service.impl.RoleServiceImpl;
 import com.codegym.lastproject.service.impl.UserServiceImpl;
+import org.springframework.beans.BeansException;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.format.Formatter;
+import org.springframework.format.FormatterRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class LastProjectApplication {
@@ -24,4 +36,31 @@ public class LastProjectApplication {
     public RoleService roleService() {
         return new RoleServiceImpl();
     }
+
+    @Bean
+    public HomeService homeService() { return new HomeServiceImpl(); }
+
+    @Bean
+    public CategoryService categoryService() { return new CategoryServiceImpl();
+    }
+
+    @Configuration
+    class WebConfig implements WebMvcConfigurer, ApplicationContextAware {
+
+        private ApplicationContext appContext;
+
+        @Override
+        public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+            appContext = applicationContext;
+        }
+
+        @Override
+        public void addFormatters(FormatterRegistry registry) {
+            CategoryService categoryService = appContext.getBean(CategoryService.class);
+            Formatter categoryFormatter = new CategoryFormatter(categoryService);
+            registry.addFormatter(categoryFormatter);
+        }
+
+    }
+
 }
